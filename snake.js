5c2c39f8c1dd335f9ee1e60326cefac6e530599c
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-10-14 21:48:53 +0700

    import image and audio

--- snake.js
+++ snake.js
@@ -1,2 +1,19 @@
 const cvs = document.getElementById("snake");
 const ctx = csv.getContext("2d");
+
+// import image
+const ground = new Image();
+ground.src = "img/ground.png";
+
+const imgFood = new Image();
+imgFood.src = "img/food.png";
+
+// import audio
+const belok = new Audio();
+belok.src = "audio/belok.mp3";
+
+const makan = new Audio();
+makan.src = "audio/makan.mp3";
+
+const gameOver = new Audio();
+gameOver.src = "audio/gameover.mp3";
